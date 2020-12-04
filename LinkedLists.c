c39d8df17e9abfa1c20cbacc1e3d53bd6168e958
alex-skxy <alex13skx@gmail.com>
2020-12-04 11:08:56 +0100

    format c file and add clion/cmake files to .gitignore

--- LinkedLists.c
+++ LinkedLists.c
@@ -1,32 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 typedef struct Elm {
-  int Nr;
-  struct Elm* pNext;
+    int Nr;
+    struct Elm *pNext;
 } struElm;
-struElm* CreateList(int Anzahl) {
-  struElm* pNew = NULL;
-  struElm* pFirst = NULL;
-  struElm* pLast = NULL;
+
+struElm *CreateList(int Anzahl) {
+  struElm *pNew = NULL;
+  struElm *pFirst = NULL;
+  struElm *pLast = NULL;
   for (int iElm = 0; iElm < Anzahl; iElm++) {
     // Element erstellen und initialisieren
-    pNew = (struElm*)malloc(sizeof(struElm));
+    pNew = (struElm *) malloc(sizeof(struElm));
     if (pNew == NULL) exit(-1);
     pNew->Nr = iElm + 1;
     pNew->pNext = NULL;
-    // Neues Element an Liste anfügen
+    // Neues Element an Liste anfï¿½gen
     if (pFirst == NULL) pFirst = pNew;
     if (pLast != NULL) pLast->pNext = pNew;
     pLast = pNew;
   }
   return pFirst;
 }
-void OutputList(struElm* pFirst) {
-  for (struElm* pElm = pFirst; pElm != NULL; pElm = pElm->pNext)
+
+void OutputList(struElm *pFirst) {
+  for (struElm *pElm = pFirst; pElm != NULL; pElm = pElm->pNext)
     printf("Nr=%i\n", pElm->Nr);
 }
+
 int main() {
-  struElm* pStart = NULL;
+  struElm *pStart = NULL;
   int AnzahlElm = 10;
   // Liste erstellen
   pStart = CreateList(AnzahlElm);
